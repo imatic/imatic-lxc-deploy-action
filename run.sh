ef3d33b7e53ff1226b2fe4b1ec90df1b79aca5bb
Jakub Babiak <jakub.babiak@imatic.cz>
2024-01-11 18:43:16 +0100

    Quiet progress

--- run.sh
+++ run.sh
@@ -8,17 +8,18 @@ cd "$(dirname "$0")"
 
 source .env.deploy
 
+DOCKER_OPTIONS="--progress=quiet"
 ENV_FILES="--env-file .env.deploy --env-file .env"
 
-docker compose ${ENV_FILES} pull
+docker compose ${DOCKER_OPTIONS} ${ENV_FILES} pull
 
-docker compose ${ENV_FILES} down || true
+docker compose ${DOCKER_OPTIONS} ${ENV_FILES} down || true
 
 if [[ -n "${PROJECT_EXTERNAL_NETWORK:-}" ]]; then
     docker 2>/dev/null 1>&2 network create --driver bridge $PROJECT_EXTERNAL_NETWORK || true
 fi
 
-docker compose ${ENV_FILES} up -d --no-build --remove-orphans
+docker compose ${DOCKER_OPTIONS} ${ENV_FILES} up -d --no-build --remove-orphans
 
 # remove all unused images
 2>/dev/null 1>&2 docker rmi $(docker images -a) || true
